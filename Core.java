e52835e20cc403bb5930858fe2f2dec569657042
Timothy Sparr <timothysparr9672@gmail.com>
2022-03-21 15:32:15 -0400

    nwe

--- Core.java
+++ Core.java
@@ -1,20 +1,30 @@
 import java.util.ArrayList;
 public class Core{
     private static ArrayList<String> playerHand = new ArrayList<String>();
-    static Deck myDeck = new Deck(3);
+    static Deck myDeck = new Deck(1);
     static Decode decode = new Decode();
     public static void main(String[] args){
         
         myDeck.shuffle();
         newHand();
-        System.out.println(playerHand.get(0));
-        System.out.println(playerHand.get(1));
+        printList(playerHand);
+        printList(myDeck.deck);
+        decode.decodeList(myDeck.deck);
     }
 
 
     public static void newHand(){
         playerHand.add(myDeck.pullCard());
         playerHand.add(myDeck.pullCard());
+        playerHand.add(myDeck.pullCard());
+        playerHand.add(myDeck.pullCard());
+    }
+
+    private static void printList(ArrayList<String> list){
+        System.out.println("\n");
+        for(int i = 0; i < list.size(); i++){
+            System.out.print(" " + list.get(i));
+        }
     }
 
 
