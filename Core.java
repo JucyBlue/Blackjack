5155be8397fe007751ffda7318026eed9e7a82d2
Timothy Sparr <timothysparr9672@gmail.com>
2022-03-22 12:16:53 -0400

    Update Core.java

--- Core.java
+++ Core.java
@@ -14,6 +14,8 @@ public class Core{
         printList(playerHand);
         System.out.println("\n");
         printList(myDeck.deck);
+        displayCards();
+        
     }
 
 
@@ -22,14 +24,31 @@ public class Core{
         playerHand.add(myDeck.pullCard());
         dealerHand.add(myDeck.pullCard());
         dealerHand.add(myDeck.pullCard());
+        dealerHand.add(myDeck.pullCard());
+        dealerHand.add(myDeck.pullCard());
     }
 
-    private static void printList(ArrayList<String> list){
+    private static String printList(ArrayList<String> list){
+        String combindedString = "";
         decodedList = decode.decodeList(list);
         System.out.println("\n");
         for(int i = 0; i < list.size(); i++){
-            System.out.print( decodedList.get(i) + "|" );
+            combindedString = combindedString + "- " +decodedList.get(i);
+            if(i+1 < list.size()) combindedString = combindedString + "\n";
         }
+        return combindedString;
+    }
+
+
+    private static void displayCards(){
+        clearConsole();
+        System.out.print("---------------Blackjack---------------\nDealers Hand:\n" 
+        + printList(dealerHand) + "\n\nYour Hand:\n" + printList(playerHand));
+    }
+
+    public static void clearConsole(){
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
     }
 
 
