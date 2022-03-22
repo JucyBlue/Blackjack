31c1223dce3af4cbeccfdf5197edc3653ae89d01
Timothy Sparr <timothysparr9672@gmail.com>
2022-03-22 12:35:44 -0400

    Update Core.java

--- Core.java
+++ Core.java
@@ -5,15 +5,16 @@ public class Core{
     private static ArrayList<String> decodedList = new ArrayList<String>();
     static Deck myDeck = new Deck(1);
     static Decode decode = new Decode();
+    static boolean hide = true;
     
     public static void main(String[] args){
         
         //myDeck.shuffle();
         newHand();
         
-        printList(playerHand);
+        printList(playerHand, hide);
         System.out.println("\n");
-        printList(myDeck.deck);
+        printList(myDeck.deck,hide);
         displayCards();
         
     }
@@ -24,11 +25,10 @@ public class Core{
         playerHand.add(myDeck.pullCard());
         dealerHand.add(myDeck.pullCard());
         dealerHand.add(myDeck.pullCard());
-        dealerHand.add(myDeck.pullCard());
-        dealerHand.add(myDeck.pullCard());
+        
     }
 
-    private static String printList(ArrayList<String> list){
+    private static String printList(ArrayList<String> list, boolean hide){
         String combindedString = "";
         decodedList = decode.decodeList(list);
         System.out.println("\n");
@@ -43,7 +43,7 @@ public class Core{
     private static void displayCards(){
         clearConsole();
         System.out.print("---------------Blackjack---------------\nDealers Hand:\n" 
-        + printList(dealerHand) + "\n\nYour Hand:\n" + printList(playerHand));
+        + printList(dealerHand, hide) + "\n\nYour Hand:\n" + printList(playerHand, hide));
     }
 
     public static void clearConsole(){
